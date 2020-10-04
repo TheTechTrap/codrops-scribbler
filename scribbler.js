97563ce7799383af4d07891b712e2d2e107f72a8
Parth Oberoi <htrapdev@gmail.com>
2020-10-04 13:39:13 +0530

    Update scribbler.js

--- scribbler.js
+++ scribbler.js
@@ -13,8 +13,8 @@ var getAll = function (selector, scope) {
 if (document.getElementsByClassName("demo").length > 0) {
   var i = 0;
   var txt = ` git remote add origin dgit://VC4NJ3nlVJJPgyJ4DScpeXx3-UnXmiasfrxiIFpJwb0/repo-name
-            <span style="color:yellow">dgit ~ $</span> git push origin master`;
-  var speed = 75;
+            dgit ~ $ git push origin master`;
+  var speed = 120;
 
   function typeItOut() {
     if (i < txt.length) {
